1a6d51b82c7a271fbdb044f07622e5178c81f011
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2020-10-23 17:50:08 +0900

    決済機能のサーバーサイド実装

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :order do
     price {Faker::Number.between(from:100, to:10000)}
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Order, type: :model do
     @order = FactoryBot.build(:order)
   end
 
-  it "priceがあれば保存できること" do
+  it "priceとtokenあれば保存できること" do
     expect(@order).to be_valid
   end
 
@@ -13,5 +13,11 @@ RSpec.describe Order, type: :model do
     @order.price = nil
     @order.valid?
     expect(@order.errors.full_messages).to include("Price can't be blank")
+  end
+
+  it "tokenが空では登録できないこと" do
+    @order.token = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("Token can't be blank")
   end
 end
